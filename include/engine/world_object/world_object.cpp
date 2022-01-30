adf747b82b9aa3b70f1ae6187ae4d092e5d64361
Pharadas <davidsudo18@gmail.com>
2022-01-29 18:58:35 -0600

    Added TexturedWorldObject

--- include/engine/world_object/world_object.cpp
+++ include/engine/world_object/world_object.cpp
@@ -8,6 +8,7 @@ WorldObject::WorldObject(Mesh* inputMesh, Material* inputMaterial) {
 
 void WorldObject::draw(VkCommandBuffer cmdBffr, int instance, MeshPushConstants pushConstants) {
 	vkCmdBindPipeline(cmdBffr, VK_PIPELINE_BIND_POINT_GRAPHICS, material->pipeline);
+	// material->setup_gpi(GPI, instance);
 	material->setup_descriptor_set(cmdBffr);
 
 	vkCmdPushConstants(cmdBffr, material->pipelineLayout, VK_SHADER_STAGE_VERTEX_BIT, 0, sizeof(MeshPushConstants), &pushConstants);
@@ -18,23 +19,52 @@ void WorldObject::draw(VkCommandBuffer cmdBffr, int instance, MeshPushConstants 
 }
 
 void WorldObject::cleanup() {
-	// material->cleanup();
-	// mesh->cleanup();
+	material->cleanup();
+	mesh->cleanup();
 }
 
-// TexturedWorldObject::TexturedWorldObject(Mesh* inputMesh, Textured_Material* inputMaterial, int texNum) : WorldObject(inputMesh, inputMaterial) {
-//     mesh = inputMesh;
-//     material = inputMaterial;
-// 	texture = texNum;
-// }
+TexturedWorldObject::TexturedWorldObject(Mesh* inputMesh, Material* inputMaterial, int texNum) : WorldObject(inputMesh, inputMaterial) {
+	mesh = inputMesh;
+	material = inputMaterial;
+	texture = texNum;
+}
 
-// void WorldObject::draw(VkCommandBuffer cmdBffr, int instance, MeshPushConstants pushConstants) {
-// 	vkCmdBindPipeline(cmdBffr, VK_PIPELINE_BIND_POINT_GRAPHICS, material->pipeline);
-// 	material->setup_descriptor_set(cmdBffr);
+void TexturedWorldObject::draw(VkCommandBuffer cmdBffr, int instance, MeshPushConstants pushConstants) {
+	// * La funcion draw() del objeto se encarga de actualizar el pushConstant con el numero de textura correcto
+	pushConstants.numOfTexture = texture;
 
-// 	vkCmdPushConstants(cmdBffr, material->pipelineLayout, VK_SHADER_STAGE_VERTEX_BIT, 0, sizeof(MeshPushConstants), &pushConstants);
-// 	VkDeviceSize offset = 0;
-// 	vkCmdBindVertexBuffers(cmdBffr, 0, 1, &(mesh->vertexBuffer), &offset);
+	vkCmdBindPipeline(cmdBffr, VK_PIPELINE_BIND_POINT_GRAPHICS, material->pipeline);
+	material->setup_descriptor_set(cmdBffr);
 
-// 	vkCmdDraw(cmdBffr, mesh->vertices.size(), 1, 0, instance);
-// }
+	vkCmdPushConstants(cmdBffr, material->pipelineLayout, VK_SHADER_STAGE_VERTEX_BIT, 0, sizeof(MeshPushConstants), &pushConstants);
+	VkDeviceSize offset = 0;
+	vkCmdBindVertexBuffers(cmdBffr, 0, 1, &(mesh->vertexBuffer), &offset);
+
+	vkCmdDraw(cmdBffr, mesh->vertices.size(), 1, 0, instance);
+}
+
+TexturedLitWorldObject::TexturedLitWorldObject(Mesh* inputMesh, Textured_Lit_Material* inputMaterial, int texNum, glm::vec3 inputColor) : WorldObject(inputMesh, inputMaterial) {
+	mesh = inputMesh;
+	material = inputMaterial;
+	texture = texNum;
+	color = inputColor;
+}
+
+void TexturedLitWorldObject::draw(VkCommandBuffer cmdBffr, int instance, MeshPushConstants pushConstants) {
+	// * La funcion draw() del objeto se encarga de actualizar el pushConstant con el numero de textura correcto
+	pushConstants.numOfTexture = texture;
+	LightingInfo lightInfo = {};
+		lightInfo.lightColor = glm::vec3(1.f, 1.f, 1.f);
+		lightInfo.objectColor = color;
+
+	vkCmdBindPipeline(cmdBffr, VK_PIPELINE_BIND_POINT_GRAPHICS, material->pipeline);
+	// * Se hace un dynamic cast para poder acceder a la informacion especifica de la clase derivada
+	dynamic_cast<Textured_Lit_Material*>(material)->lightInfo.lightColor = color;
+	material->setup_descriptor_set(cmdBffr);
+
+	vkCmdPushConstants(cmdBffr, material->pipelineLayout, VK_SHADER_STAGE_VERTEX_BIT, 0, sizeof(MeshPushConstants), &pushConstants);
+	VkDeviceSize offset = 0;
+	vkCmdBindVertexBuffers(cmdBffr, 0, 1, &(mesh->vertexBuffer), &offset);
+
+	vkCmdDraw(cmdBffr, mesh->vertices.size(), 1, 0, instance);
+}

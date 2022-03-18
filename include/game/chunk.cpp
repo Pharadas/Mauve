0ce672684708c35165925fa3067e34e6edbdeb8a
Pharadas <davidsudo18@gmail.com>
2022-03-18 17:08:08 -0600

    Facade Commented for changes in mesh constructor

--- include/game/chunk.cpp
+++ include/game/chunk.cpp
@@ -1,28 +1,28 @@
-#include <game/chunk.hpp>
+// #include <game/chunk.hpp>
 
-Chunk::Chunk(glm::vec2 position, siv::PerlinNoise noise, VkCommandPool commandPool, VkQueue graphicsQueue, Material* inputMaterial) {
-   std::vector<Vertex> chunkVertices;
+// Chunk::Chunk(glm::vec2 position, siv::PerlinNoise noise, VkCommandPool commandPool, VkQueue graphicsQueue, Material* inputMaterial) {
+//    std::vector<Vertex> chunkVertices;
 
-   for (int y = 0; y < chunkSize; ++y) {
-		for (int x = 0; x < chunkSize; ++x) {
-			const double height = noise.octave2D_01((x), (y), 4);
+//    for (int y = 0; y < chunkSize; ++y) {
+// 		for (int x = 0; x < chunkSize; ++x) {
+// 			const double height = noise.octave2D_01((x), (y), 4, .1);
 
-			world[x][y] = int(height * 10);
+// 			world[x][y] = int(height * 10);
 
-         for (auto i : singleCube) {
-            glm::vec3 originalPosition = glm::vec3(i.pos.x * 0.5, i.pos.y * 0.5, i.pos.z * 0.5);
-            glm::vec3 translatedPosition;
+//          for (auto i : singleCube) {
+//             glm::vec3 originalPosition = glm::vec3(i.pos.x * 0.5, i.pos.y * 0.5, i.pos.z * 0.5);
+//             glm::vec3 translatedPosition;
 
-            translatedPosition.x = originalPosition.x + x;
-            translatedPosition.y = originalPosition.y + world[x][y];
-            translatedPosition.z = originalPosition.z + y;
+//             translatedPosition.x = originalPosition.x + x;
+//             translatedPosition.y = originalPosition.y + world[x][y];
+//             translatedPosition.z = originalPosition.z + y;
 
-            chunkVertices.push_back({{translatedPosition}, i.normal, i.texCoord});
-         }
-		}
-	}
+//             chunkVertices.push_back({{translatedPosition}, i.normal, i.texCoord});
+//          }
+// 		}
+// 	}
 
-   chunkMesh.reset(new Mesh(chunkVertices, commandPool, graphicsQueue));
+//    // chunkMesh.reset(new Mesh(chunkVertices, commandPool, graphicsQueue));
 
-   chunkRenderableObject.reset(new TexturedWorldObject(chunkMesh.get(), inputMaterial, 0));
-}
+//    chunkRenderableObject.reset(new TexturedWorldObject(chunkMesh.get(), inputMaterial, 0));
+// }

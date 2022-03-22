619234dacea9ad12a965cc5b3ae58058a17bdb69
Pharadas <davidsudo18@gmail.com>
2022-03-21 19:12:42 -0600

    Re-added and fixed cubes

--- include/game/chunk.cpp
+++ include/game/chunk.cpp
@@ -1,28 +1,88 @@
-// #include <game/chunk.hpp>
+#include <game/chunk.hpp>
 
-// Chunk::Chunk(glm::vec2 position, siv::PerlinNoise noise, VkCommandPool commandPool, VkQueue graphicsQueue, Material* inputMaterial) {
-//    std::vector<Vertex> chunkVertices;
+Chunk::Chunk(glm::vec2 position, siv::PerlinNoise noise) {
+   mesh = std::make_shared<Mesh>();
 
-//    for (int y = 0; y < chunkSize; ++y) {
-// 		for (int x = 0; x < chunkSize; ++x) {
-// 			const double height = noise.octave2D_01((x), (y), 4, .1);
+   float vertices[] = {
+      // * atras ./
+      0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+      0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+      -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+      -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+      -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+      0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
 
-// 			world[x][y] = int(height * 10);
+      // * frente ./
+      -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+      0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+      0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+      0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+      -0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+      -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
 
-//          for (auto i : singleCube) {
-//             glm::vec3 originalPosition = glm::vec3(i.pos.x * 0.5, i.pos.y * 0.5, i.pos.z * 0.5);
-//             glm::vec3 translatedPosition;
+      // * izquierda ./
+      -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+      -0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+      -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+      -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+      -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+      -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
 
-//             translatedPosition.x = originalPosition.x + x;
-//             translatedPosition.y = originalPosition.y + world[x][y];
-//             translatedPosition.z = originalPosition.z + y;
+      // * Derecha ./
+      0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+      0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+      0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+      0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+      0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+      0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
 
-//             chunkVertices.push_back({{translatedPosition}, i.normal, i.texCoord});
-//          }
-// 		}
-// 	}
+      // * Abajo ./
+      -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+      0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+      0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+      0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+      -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+      -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
 
-//    // chunkMesh.reset(new Mesh(chunkVertices, commandPool, graphicsQueue));
+      // * Arriba
+      0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+      -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+      0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+      -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+      -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+      0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+   };
 
-//    chunkRenderableObject.reset(new TexturedWorldObject(chunkMesh.get(), inputMaterial, 0));
-// }
+   for (int y = 0; y < chunkSize; y++) {
+		for (int x = 0; x < chunkSize; x++) {
+			const double height = noise.octave2D_01((x + (position.x * chunkSize)) * (1. / chunkSize), (y + (position.y * chunkSize)) * (1. / chunkSize), 8);
+
+			world[x][y] = int(height * 10);
+
+         for (int r = 0; r < world[x][y]; r++) {
+            for (int i = 0; i < 180; i += 5) {
+               glm::vec3 originalPosition = glm::vec3(vertices[i], vertices[i + 1], vertices[i + 2]);
+               glm::vec3 translatedPosition;
+
+               translatedPosition.x = originalPosition.x + x;
+               translatedPosition.y = originalPosition.y + r;
+               translatedPosition.z = originalPosition.z + y;
+
+               chunkVertices.push_back({{translatedPosition}, {0, 0, 0}, {vertices[i + 3], vertices[i + 4]}});
+            }
+         }
+         // for (auto i : singleCube) {
+         //    glm::vec3 originalPosition = glm::vec3(i.pos.x * 0.5, i.pos.y * 0.5, i.pos.z * 0.5);
+         //    glm::vec3 translatedPosition;
+
+         //    translatedPosition.x = originalPosition.x + x;
+         //    translatedPosition.y = originalPosition.y + world[x][y];
+         //    translatedPosition.z = originalPosition.z + y;
+
+         //    chunkVertices.push_back({{translatedPosition}, i.normal, i.texCoord});
+         // }
+		}
+	}
+
+   mesh->setVertices(chunkVertices);
+}
